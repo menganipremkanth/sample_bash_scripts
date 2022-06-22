095c763bfb63745764d2b3d11c45947c818475d4
premkanth mengani <mengani.premkanth@gmail.com>
2022-06-22 14:02:59 +0530

    added exit to basic script

--- basic_script.bash
+++ basic_script.bash
@@ -2,7 +2,7 @@
 echo "Hello, this is basic script"
 echo "you currently in this directory $PWD"
 echo "below files are present under $PWD currently"
-ls -lrt $PWD/
+ils -lrt $PWD/
 
 #list hidden files aswell.
 echo "below are hidden files"
@@ -10,4 +10,6 @@ ls -lart $PWD/
 
 
 echo "you are executing script called $0"
+
+exit
 
