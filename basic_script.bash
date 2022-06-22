b7a09d56bbd6cd97123bf1ee17960201ec405a9f
premkanth mengani <premmengani@pk.local>
2022-06-22 10:32:48 +0530

    second commit - added command to list hiddent files in pwd

--- basic_script.bash
+++ basic_script.bash
@@ -3,3 +3,7 @@ echo "Hello, this is basic script"
 echo "you currently in this directory $PWD"
 echo "below files are present under $PWD currently"
 ls -lrt $PWD/
+
+#list hidden files aswell.
+echo "below are hidden files"
+ls -lart $PWD/
